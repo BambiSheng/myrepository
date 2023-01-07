e9dec0d494b310fee06db021d8ecda1175702be0
BambiSheng <115133376+BambiSheng@users.noreply.github.com>
2023-01-07 14:20:19 +0800

    upd 23.1.7

--- pointer and array/test.cpp
+++ pointer and array/test.cpp
@@ -1,19 +1,11 @@
-#include <iostream> 
-using namespace std;
-#include <stdio.h>
-int main()
-{
-FILE *fp;
-char i, a[10]="tsinghua";
-static char b[10];
-fp = fopen("d.txt", "w");
-for(i=0; i<6; i++)
-fprintf(fp, "%c", a[i]);
-fclose(fp);
-fp = fopen("d.txt", "r");
-fseek(fp, 2, SEEK_SET);
-fgets(b, 4, fp);
-fclose(fp);
-cout << b;
-return 0;
+#include<stdio.h>
+#include<math.h>
+int main(){
+  double n=99;
+  double ans;
+  for(int i=1;i<=n;i++){
+    ans+=1/(sqrt(n)+sqrt(i));
+  }
+  ans*=1/sqrt(n);
+  printf("%lf",ans);
 }
